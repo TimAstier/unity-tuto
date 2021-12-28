7327170c6aac68f69e9a9f86c073182fcee657f4
Timothée Astier <timothee.astier@essec.edu>
2021-12-29 00:03:29 +0100

    End E23

--- Assets/Scripts/Models/World.cs
+++ Assets/Scripts/Models/World.cs
@@ -10,204 +10,204 @@ using UnityEngine;
 
 public class World {
 
-    // A two-dimensional array to hold our tile data.
-    Tile[,] tiles;
-    List<Character> characters;
+  // A two-dimensional array to hold our tile data.
+  Tile[,] tiles;
+  List<Character> characters;
 
-    Path_TileGraph tileGraph;
+  public Path_TileGraph tileGraph;
 
-    Dictionary<string, Furniture> furniturePrototypes;
+  Dictionary<string, Furniture> furniturePrototypes;
 
-    // The tile width of the world.
-    public int Width { get; protected set; }
+  // The tile width of the world.
+  public int Width { get; protected set; }
 
-    // The tile height of the world
-    public int Height { get; protected set; }
+  // The tile height of the world
+  public int Height { get; protected set; }
 
-    Action<Furniture> cbFurnitureCreated;
-    Action<Character> cbCharacterCreated;
-    Action<Tile> cbTileChanged;
+  Action<Furniture> cbFurnitureCreated;
+  Action<Character> cbCharacterCreated;
+  Action<Tile> cbTileChanged;
 
-    public JobQueue jobQueue;
+  public JobQueue jobQueue;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="World"/> class.
-    /// </summary>
-    /// <param name="width">Width in tiles.</param>
-    /// <param name="height">Height in tiles.</param>
-    public World(int width = 100, int height = 100) {
-        jobQueue = new JobQueue();
+  /// <summary>
+  /// Initializes a new instance of the <see cref="World"/> class.
+  /// </summary>
+  /// <param name="width">Width in tiles.</param>
+  /// <param name="height">Height in tiles.</param>
+  public World(int width = 100, int height = 100) {
+    jobQueue = new JobQueue();
 
-        Width = width;
-        Height = height;
+    Width = width;
+    Height = height;
 
-        tiles = new Tile[Width, Height];
+    tiles = new Tile[Width, Height];
 
-        for (int x = 0; x < Width; x++) {
-            for (int y = 0; y < Height; y++) {
-                tiles[x, y] = new Tile(this, x, y);
-                tiles[x, y].RegisterTileTypeChangedCallback(OnTileChanged);
-            }
+    for (int x = 0; x < Width; x++) {
+      for (int y = 0; y < Height; y++) {
+        tiles[x, y] = new Tile(this, x, y);
+        tiles[x, y].RegisterTileTypeChangedCallback(OnTileChanged);
+      }
+    }
+
+    Debug.Log("World created with " + (Width * Height) + " tiles.");
+
+    CreateFurniturePrototypes();
+
+    characters = new List<Character>();
+  }
+
+  public void Update(float deltaTime) {
+    foreach (Character c in characters) {
+      c.Update(deltaTime);
+    }
+  }
+
+  public Character CreateCharacter(Tile t) {
+    Character c = new Character(t);
+
+    characters.Add(c);
+
+    if (cbCharacterCreated != null) {
+      cbCharacterCreated(c);
+    }
+
+    return c;
+  }
+
+  void CreateFurniturePrototypes() {
+    furniturePrototypes = new Dictionary<string, Furniture>();
+
+    furniturePrototypes.Add("Wall",
+      Furniture.CreatePrototype(
+                "Wall",
+                0,  // Impassable
+                1,  // Width
+                1,  // Height
+                true // Links to neighbours and "sort of" becomes part of a large object
+              )
+    );
+  }
+
+  /// <summary>
+  /// A function for testing out the system
+  /// </summary>
+  public void RandomizeTiles() {
+    Debug.Log("RandomizeTiles");
+    for (int x = 0; x < Width; x++) {
+      for (int y = 0; y < Height; y++) {
+
+        if (UnityEngine.Random.Range(0, 2) == 0) {
+          tiles[x, y].Type = TileType.Empty;
+        } else {
+          tiles[x, y].Type = TileType.Floor;
         }
 
-        Debug.Log("World created with " + (Width * Height) + " tiles.");
-
-        CreateFurniturePrototypes();
-
-        characters = new List<Character>();
+      }
     }
+  }
 
-    public void Update(float deltaTime) {
-        foreach (Character c in characters) {
-            c.Update(deltaTime);
-        }
-    }
+  public void SetupPathfindingExample() {
+    int l = Width / 2 - 5;
+    int b = Height / 2 - 5;
 
-    public Character CreateCharacter(Tile t) {
-        Character c = new Character(t);
+    for (int x = l - 5; x < l + 15; x++) {
+      for (int y = b - 5; y < b + 15; y++) {
+        tiles[x, y].Type = TileType.Floor;
 
-        characters.Add(c);
-
-        if (cbCharacterCreated != null) {
-            cbCharacterCreated(c);
+        if (x == l || x == (l + 9) || y == b || y == (b + 9)) {
+          if ((x != l + 9) && y != (b + 4)) {
+            PlaceFurniture("Wall", tiles[x, y]);
+          }
         }
 
-        return c;
+      }
+    }
+  }
+
+  /// <summary>
+  /// Gets the tile data at x and y.
+  /// </summary>
+  /// <returns>The <see cref="Tile"/>.</returns>
+  /// <param name="x">The x coordinate.</param>
+  /// <param name="y">The y coordinate.</param>
+  public Tile GetTileAt(int x, int y) {
+    if (x > Width || x < 0 || y > Height || y < 0) {
+      Debug.LogError("Tile (" + x + "," + y + ") is out of range.");
+      return null;
+    }
+    return tiles[x, y];
+  }
+
+
+  public void PlaceFurniture(string objectType, Tile t) {
+    //Debug.Log("PlaceInstalledObject");
+    // TODO: This function assumes 1x1 tiles -- change this later!
+
+    if (furniturePrototypes.ContainsKey(objectType) == false) {
+      Debug.LogError("furniturePrototypes doesn't contain a proto for key: " + objectType);
+      return;
     }
 
-    void CreateFurniturePrototypes() {
-        furniturePrototypes = new Dictionary<string, Furniture>();
+    Furniture obj = Furniture.PlaceInstance(furniturePrototypes[objectType], t);
 
-        furniturePrototypes.Add("Wall",
-          Furniture.CreatePrototype(
-                    "Wall",
-                    0,  // Impassable
-                    1,  // Width
-                    1,  // Height
-                    true // Links to neighbours and "sort of" becomes part of a large object
-                  )
-        );
+    if (obj == null) {
+      // Failed to place object -- most likely there was already something there.
+      return;
     }
 
-    /// <summary>
-    /// A function for testing out the system
-    /// </summary>
-    public void RandomizeTiles() {
-        Debug.Log("RandomizeTiles");
-        for (int x = 0; x < Width; x++) {
-            for (int y = 0; y < Height; y++) {
+    if (cbFurnitureCreated != null) {
+      cbFurnitureCreated(obj);
+      InvalidateTileGraph();
+    }
+  }
 
-                if (UnityEngine.Random.Range(0, 2) == 0) {
-                    tiles[x, y].Type = TileType.Empty;
-                } else {
-                    tiles[x, y].Type = TileType.Floor;
-                }
+  public void RegisterFurnitureCreated(Action<Furniture> callbackfunc) {
+    cbFurnitureCreated += callbackfunc;
+  }
 
-            }
-        }
+  public void UnregisterFurnitureCreated(Action<Furniture> callbackfunc) {
+    cbFurnitureCreated -= callbackfunc;
+  }
+
+  public void RegisterCharacterCreated(Action<Character> callbackfunc) {
+    cbCharacterCreated += callbackfunc;
+  }
+
+  public void UnregisterCharacterCreated(Action<Character> callbackfunc) {
+    cbCharacterCreated -= callbackfunc;
+  }
+
+  public void RegisterTileChanged(Action<Tile> callbackfunc) {
+    cbTileChanged += callbackfunc;
+  }
+
+  public void UnregisterTileChanged(Action<Tile> callbackfunc) {
+    cbTileChanged -= callbackfunc;
+  }
+
+  void OnTileChanged(Tile t) {
+    if (cbTileChanged == null) {
+      return;
+    }
+    cbTileChanged(t);
+    InvalidateTileGraph();
+  }
+
+  public void InvalidateTileGraph() {
+    tileGraph = null;
+  }
+
+  public bool IsFurniturePlacementValid(string furnitureType, Tile t) {
+    return furniturePrototypes[furnitureType].IsValidPosition(t);
+  }
+
+  public Furniture GetFurniturePrototype(string objectType) {
+    if (furniturePrototypes.ContainsKey(objectType) == false) {
+      Debug.LogError("No prototype for :" + objectType);
+      return null;
     }
 
-    public void SetupPathfindingExample() {
-        int l = Width / 2 - 5;
-        int b = Height / 2 - 5;
-
-        for (int x = l - 5; x < l + 15; x++) {
-            for (int y = b-5; y < b+ 15; y++) {
-                tiles[x, y].Type = TileType.Floor;
-
-                if (x == l || x== (l + 9) || y == b || y == (b+9)) {
-                    if ((x != l+9) && y!= (b+4) ) {
-                        PlaceFurniture("Wall", tiles[x, y]);
-                    }
-                }
-
-            }
-        } 
-    }
-
-    /// <summary>
-    /// Gets the tile data at x and y.
-    /// </summary>
-    /// <returns>The <see cref="Tile"/>.</returns>
-    /// <param name="x">The x coordinate.</param>
-    /// <param name="y">The y coordinate.</param>
-    public Tile GetTileAt(int x, int y) {
-        if (x > Width || x < 0 || y > Height || y < 0) {
-            Debug.LogError("Tile (" + x + "," + y + ") is out of range.");
-            return null;
-        }
-        return tiles[x, y];
-    }
-
-
-    public void PlaceFurniture(string objectType, Tile t) {
-        //Debug.Log("PlaceInstalledObject");
-        // TODO: This function assumes 1x1 tiles -- change this later!
-
-        if (furniturePrototypes.ContainsKey(objectType) == false) {
-            Debug.LogError("furniturePrototypes doesn't contain a proto for key: " + objectType);
-            return;
-        }
-
-        Furniture obj = Furniture.PlaceInstance(furniturePrototypes[objectType], t);
-
-        if (obj == null) {
-            // Failed to place object -- most likely there was already something there.
-            return;
-        }
-
-        if (cbFurnitureCreated != null) {
-            cbFurnitureCreated(obj);
-            InvalidateTileGraph();
-        }
-    }
-
-    public void RegisterFurnitureCreated(Action<Furniture> callbackfunc) {
-        cbFurnitureCreated += callbackfunc;
-    }
-
-    public void UnregisterFurnitureCreated(Action<Furniture> callbackfunc) {
-        cbFurnitureCreated -= callbackfunc;
-    }
-
-    public void RegisterCharacterCreated(Action<Character> callbackfunc) {
-        cbCharacterCreated += callbackfunc;
-    }
-
-    public void UnregisterCharacterCreated(Action<Character> callbackfunc) {
-        cbCharacterCreated -= callbackfunc;
-    }
-
-    public void RegisterTileChanged(Action<Tile> callbackfunc) {
-        cbTileChanged += callbackfunc;
-    }
-
-    public void UnregisterTileChanged(Action<Tile> callbackfunc) {
-        cbTileChanged -= callbackfunc;
-    }
-
-    void OnTileChanged(Tile t) {
-        if (cbTileChanged == null) {
-            return;
-        }
-        cbTileChanged(t);
-        InvalidateTileGraph();
-    }
-
-    public void InvalidateTileGraph() {
-        tileGraph = null;
-    }
-
-    public bool IsFurniturePlacementValid(string furnitureType, Tile t) {
-        return furniturePrototypes[furnitureType].IsValidPosition(t);
-    }
-
-    public Furniture GetFurniturePrototype(string objectType) {
-        if (furniturePrototypes.ContainsKey(objectType) == false) {
-            Debug.LogError("No prototype for :" + objectType);
-            return null;
-        }
-
-        return furniturePrototypes[objectType];
-    }
+    return furniturePrototypes[objectType];
+  }
 }
--- Assets/Scripts/Pathfinding/Path_TileGraph.cs
+++ Assets/Scripts/Pathfinding/Path_TileGraph.cs
@@ -4,54 +4,54 @@ using UnityEngine;
 
 public class Path_TileGraph {
 
-    
 
-    // This class constructs a simple path-finding compatible graph
-    // of our world. Each tile is a node and each walklable neighbour
-    // is linked though and edge connection.
 
-    Dictionary<Tile, Path_Node<Tile>> nodes;
+  // This class constructs a simple path-finding compatible graph
+  // of our world. Each tile is a node and each walklable neighbour
+  // is linked though and edge connection.
 
-    public Path_TileGraph(World world) {
-        Debug.Log("Path_TileGraph");
-        nodes = new Dictionary<Tile, Path_Node<Tile>>();
+  public Dictionary<Tile, Path_Node<Tile>> nodes;
 
-        for (int x = 0; x < world.Width; x++) {
-            for (int y = 0; y < world.Height; y++) {
-                Tile t = world.GetTileAt(x, y);
-                if (t.movementCost > 0) {
-                    Path_Node<Tile> n = new Path_Node<Tile>();
-                    n.data = t;
-                    nodes.Add(t, n);
-                }
-            }
+  public Path_TileGraph(World world) {
+    Debug.Log("Path_TileGraph");
+    nodes = new Dictionary<Tile, Path_Node<Tile>>();
+
+    for (int x = 0; x < world.Width; x++) {
+      for (int y = 0; y < world.Height; y++) {
+        Tile t = world.GetTileAt(x, y);
+        if (t.movementCost > 0) {
+          Path_Node<Tile> n = new Path_Node<Tile>();
+          n.data = t;
+          nodes.Add(t, n);
         }
-
-        Debug.Log("Path_TileGraph: Created " + nodes.Count + "nodes.");
-
-        int edgeCount = 0;
-
-        foreach (Tile t in nodes.Keys) {
-            Path_Node<Tile> n = nodes[t];
-
-            List<Path_Edge<Tile>> edges = new List<Path_Edge<Tile>>();
-
-            // Get a list of neighbours
-
-            Tile[] neighbours = t.GetNeighbours(true);
-
-            for (int i = 0; i < neighbours.Length; i++) {
-                if (neighbours[i] != null && neighbours[i].movementCost > 0) {
-                    Path_Edge<Tile> e = new Path_Edge<Tile>();
-                    e.cost = neighbours[i].movementCost;
-                    e.node = nodes[neighbours[i]];
-                    edges.Add(e);
-                    edgeCount++;
-                }
-            }
-
-            n.edges = edges.ToArray();
-        }
-        Debug.Log("Path_TileGraph: Created " + edgeCount + "edges.");
+      }
     }
+
+    Debug.Log("Path_TileGraph: Created " + nodes.Count + "nodes.");
+
+    int edgeCount = 0;
+
+    foreach (Tile t in nodes.Keys) {
+      Path_Node<Tile> n = nodes[t];
+
+      List<Path_Edge<Tile>> edges = new List<Path_Edge<Tile>>();
+
+      // Get a list of neighbours
+
+      Tile[] neighbours = t.GetNeighbours(true);
+
+      for (int i = 0; i < neighbours.Length; i++) {
+        if (neighbours[i] != null && neighbours[i].movementCost > 0) {
+          Path_Edge<Tile> e = new Path_Edge<Tile>();
+          e.cost = neighbours[i].movementCost;
+          e.node = nodes[neighbours[i]];
+          edges.Add(e);
+          edgeCount++;
+        }
+      }
+
+      n.edges = edges.ToArray();
+    }
+    Debug.Log("Path_TileGraph: Created " + edgeCount + "edges.");
+  }
 }
