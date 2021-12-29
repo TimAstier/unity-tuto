ac651d9b6080d55dc6d876d75ee691afeb06189d
Timothée Astier <timothee.astier@essec.edu>
2021-12-29 16:28:40 +0100

    Place README at the right place

--- Assets/Scripts/Controllers/BuildModeController.cs
+++ Assets/Scripts/Controllers/BuildModeController.cs
@@ -1,84 +1,80 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BuildModeController : MonoBehaviour {
 
-	bool     buildModeIsObjects = false;
-	TileType buildModeTile = TileType.Floor;
-	string   buildModeObjectType;
+  bool buildModeIsObjects = false;
+  TileType buildModeTile = TileType.Floor;
+  string buildModeObjectType;
 
-	// Use this for initialization
-	void Start () {
-	}
+  // Use this for initialization
+  void Start() {
+  }
 
-	public void SetMode_BuildFloor( ) {
-		buildModeIsObjects = false;
-		buildModeTile = TileType.Floor;
-	}
-	
-	public void SetMode_Bulldoze( ) {
-		buildModeIsObjects = false;
-		buildModeTile = TileType.Empty;
-	}
+  public void SetMode_BuildFloor() {
+    buildModeIsObjects = false;
+    buildModeTile = TileType.Floor;
+  }
 
-	public void SetMode_BuildFurniture( string objectType ) {
-		// Wall is not a Tile!  Wall is an "Furniture" that exists on TOP of a tile.
-		buildModeIsObjects = true;
-		buildModeObjectType = objectType;
-	}
+  public void SetMode_Bulldoze() {
+    buildModeIsObjects = false;
+    buildModeTile = TileType.Empty;
+  }
 
-	public void DoPathfindingTest() {
-		WorldController.Instance.world.SetupPathfindingExample();
-	}
+  public void SetMode_BuildFurniture(string objectType) {
+    // Wall is not a Tile!  Wall is an "Furniture" that exists on TOP of a tile.
+    buildModeIsObjects = true;
+    buildModeObjectType = objectType;
+  }
 
-	public void DoBuild( Tile t ) {
-		if(buildModeIsObjects == true) {
-			// Create the Furniture and assign it to the tile
+  public void DoPathfindingTest() {
+    WorldController.Instance.world.SetupPathfindingExample();
+  }
 
-			// FIXME: This instantly builds the furnite:
-			//WorldController.Instance.World.PlaceFurniture( buildModeObjectType, t );
+  public void DoBuild(Tile t) {
+    if (buildModeIsObjects == true) {
+      // Create the Furniture and assign it to the tile
 
-			// Can we build the furniture in the selected tile?
-			// Run the ValidPlacement function!
+      // FIXME: This instantly builds the furnite:
+      //WorldController.Instance.World.PlaceFurniture( buildModeObjectType, t );
 
-			string furnitureType = buildModeObjectType;
+      // Can we build the furniture in the selected tile?
+      // Run the ValidPlacement function!
 
-			if( 
-				WorldController.Instance.world.IsFurniturePlacementValid( furnitureType, t ) &&
-				t.pendingFurnitureJob == null
-			) {
-				// This tile position is valid for this furniture
-				// Create a job for it to be build
+      string furnitureType = buildModeObjectType;
 
-				Job j = new Job(t, furnitureType, (theJob) => { 
-					WorldController.Instance.world.PlaceFurniture( furnitureType, theJob.tile );
+      if (
+        WorldController.Instance.world.IsFurniturePlacementValid(furnitureType, t) &&
+        t.pendingFurnitureJob == null
+      ) {
+        // This tile position is valid for this furniture
+        // Create a job for it to be build
 
-					// FIXME: I don't like having to manually and explicitly set
-					// flags that preven conflicts. It's too easy to forget to set/clear them!
-					t.pendingFurnitureJob = null;
-				}
-				);
+        Job j = new Job(t, furnitureType, (theJob) => {
+          WorldController.Instance.world.PlaceFurniture(furnitureType, theJob.tile);
 
+          // FIXME: I don't like having to manually and explicitly set
+          // flags that preven conflicts. It's too easy to forget to set/clear them!
+          t.pendingFurnitureJob = null;
+        }
+        );
 
-				// FIXME: I don't like having to manually and explicitly set
-				// flags that preven conflicts. It's too easy to forget to set/clear them!
-				t.pendingFurnitureJob = j;
-				j.RegisterJobCancelCallback( (theJob) => { theJob.tile.pendingFurnitureJob = null; } );
+        // FIXME: I don't like having to manually and explicitly set
+        // flags that preven conflicts. It's too easy to forget to set/clear them!
+        t.pendingFurnitureJob = j;
+        j.RegisterJobCancelCallback((theJob) => { theJob.tile.pendingFurnitureJob = null; });
 
-				// Add the job to the queue
-				WorldController.Instance.world.jobQueue.Enqueue( j );
+        // Add the job to the queue
+        WorldController.Instance.world.jobQueue.Enqueue(j);
 
-			}
+      }
 
+    } else {
+      // We are in tile-changing mode.
+      t.Type = buildModeTile;
+    }
 
+  }
 
-		}
-		else {
-			// We are in tile-changing mode.
-			t.Type = buildModeTile;
-		}
-
-	}
-	
 }
