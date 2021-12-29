784fa24f0a86b08ed972472bffd6f3afce842a77
Timothée Astier <timothee.astier@essec.edu>
2021-12-29 13:37:41 +0100

    End E25

--- Assets/Scripts/Controllers/JobSpriteController.cs
+++ Assets/Scripts/Controllers/JobSpriteController.cs
@@ -1,65 +1,70 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class JobSpriteController : MonoBehaviour {
 
-	// This bare-bones controller is mostly just going to piggyback
-	// on FurnitureSpriteController because we don't yet fully know
-	// what our job system is going to look like in the end.
+  // This bare-bones controller is mostly just going to piggyback
+  // on FurnitureSpriteController because we don't yet fully know
+  // what our job system is going to look like in the end.
 
-	FurnitureSpriteController fsc;
-	Dictionary<Job, GameObject> jobGameObjectMap;
+  FurnitureSpriteController fsc;
+  Dictionary<Job, GameObject> jobGameObjectMap;
 
-	// Use this for initialization
-	void Start () {
-		jobGameObjectMap = new Dictionary<Job, GameObject>();
-		fsc = GameObject.FindObjectOfType<FurnitureSpriteController>();
+  // Use this for initialization
+  void Start() {
+    jobGameObjectMap = new Dictionary<Job, GameObject>();
+    fsc = GameObject.FindObjectOfType<FurnitureSpriteController>();
 
-		// FIXME: No such thing as a job queue yet!
-		WorldController.Instance.world.jobQueue.RegisterJobCreationCallback(OnJobCreated);
-	}
+    // FIXME: No such thing as a job queue yet!
+    WorldController.Instance.world.jobQueue.RegisterJobCreationCallback(OnJobCreated);
+  }
 
-	void OnJobCreated(Job job) {
-		// FIXME: We can only do furniture-building jobs.
+  void OnJobCreated(Job job) {
+    // FIXME: We can only do furniture-building jobs.
 
-		// TODO: Sprite
+    // TODO: Sprite
 
-		GameObject job_go = new GameObject();
+    GameObject job_go = new GameObject();
 
-		if(jobGameObjectMap.ContainsKey(job)) {
-			Debug.LogError("OnJobCreated for a jobGO that already exists -- most likely a job being RE-QUEUED, as opposed to created.");
-			return;
-		}
 
-		// Add our tile/GO pair to the dictionary.
-		jobGameObjectMap.Add( job, job_go );
+    if (jobGameObjectMap.ContainsKey(job)) {
+      return;
+    }
 
-		job_go.name = "JOB_" + job.jobObjectType + "_" + job.tile.X + "_" + job.tile.Y;
-		job_go.transform.position = new Vector3( job.tile.X, job.tile.Y, 0);
-		job_go.transform.SetParent(this.transform, true);
+    if (jobGameObjectMap.ContainsKey(job)) {
+      Debug.LogError("OnJobCreated for a jobGO that already exists -- most likely a job being RE-QUEUED, as opposed to created.");
+      return;
+    }
 
-		SpriteRenderer sr = job_go.AddComponent<SpriteRenderer>();
-		sr.sprite = fsc.GetSpriteForFurniture( job.jobObjectType );
-		sr.color = new Color( 0.5f, 1f, 0.5f, 0.25f );
-		sr.sortingLayerName = "Jobs";
+    // Add our tile/GO pair to the dictionary.
+    jobGameObjectMap.Add(job, job_go);
 
-		job.RegisterJobCompleteCallback(OnJobEnded);
-		job.RegisterJobCancelCallback(OnJobEnded);
-	}
+    job_go.name = "JOB_" + job.jobObjectType + "_" + job.tile.X + "_" + job.tile.Y;
+    job_go.transform.position = new Vector3(job.tile.X, job.tile.Y, 0);
+    job_go.transform.SetParent(this.transform, true);
 
-	void OnJobEnded(Job job) {
-		// This executes whether a job was COMPLETED or CANCELLED
+    SpriteRenderer sr = job_go.AddComponent<SpriteRenderer>();
+    sr.sprite = fsc.GetSpriteForFurniture(job.jobObjectType);
+    sr.color = new Color(0.5f, 1f, 0.5f, 0.25f);
+    sr.sortingLayerName = "Jobs";
 
-		// FIXME: We can only do furniture-building jobs.
+    job.RegisterJobCompleteCallback(OnJobEnded);
+    job.RegisterJobCancelCallback(OnJobEnded);
+  }
 
-		GameObject job_go = jobGameObjectMap[job];
+  void OnJobEnded(Job job) {
+    // This executes whether a job was COMPLETED or CANCELLED
 
-		job.UnregisterJobCompleteCallback(OnJobEnded);
-		job.UnregisterJobCancelCallback(OnJobEnded);
+    // FIXME: We can only do furniture-building jobs.
 
-		Destroy(job_go);
+    GameObject job_go = jobGameObjectMap[job];
 
-	}
+    job.UnregisterJobCompleteCallback(OnJobEnded);
+    job.UnregisterJobCancelCallback(OnJobEnded);
+
+    Destroy(job_go);
+
+  }
 
 
 
--- Assets/Scripts/Models/Tile.cs
+++ Assets/Scripts/Models/Tile.cs
@@ -3,9 +3,9 @@
 //		http://quill18.com
 //=======================================================================
 
-using UnityEngine;
-using System.Collections;
 using System;
+using System.Collections;
+using UnityEngine;
 
 // TileType is the base type of the tile. In some tile-based games, that might be
 // the terrain type. For us, we only need to differentiate between empty space
@@ -14,148 +14,147 @@ using System;
 public enum TileType { Empty, Floor };
 
 public class Tile {
-	private TileType _type = TileType.Empty;
-	public TileType Type {
-		get { return _type; }
-		set {
-			TileType oldType = _type;
-			_type = value;
-			// Call the callback and let things know we've changed.
+  private TileType _type = TileType.Empty;
+  public TileType Type {
+    get { return _type; }
+    set {
+      TileType oldType = _type;
+      _type = value;
+      // Call the callback and let things know we've changed.
 
-			if(cbTileChanged != null && oldType != _type) {
-				cbTileChanged(this);
-			}
-		}
-	}
+      if (cbTileChanged != null && oldType != _type) {
+        cbTileChanged(this);
+      }
+    }
+  }
 
-	// LooseObject is something like a drill or a stack of metal sitting on the floor
-	Inventory inventory;
+  // LooseObject is something like a drill or a stack of metal sitting on the floor
+  Inventory inventory;
 
-	// Furniture is something like a wall, door, or sofa.
-	public Furniture furniture {
-		get; protected set;
-	}
+  // Furniture is something like a wall, door, or sofa.
+  public Furniture furniture {
+    get; protected set;
+  }
 
-	// FIXME: This seems like a terrible way to flag if a job is pending
-	// on a tile.  This is going to be prone to errors in set/clear.
-	public Job pendingFurnitureJob;
+  // FIXME: This seems like a terrible way to flag if a job is pending
+  // on a tile.  This is going to be prone to errors in set/clear.
+  public Job pendingFurnitureJob;
 
-	// We need to know the context in which we exist. Probably. Maybe.
-	public World world { get; protected set; }
+  // We need to know the context in which we exist. Probably. Maybe.
+  public World world { get; protected set; }
 
-	public int X { get; protected set; }
-	public int Y { get; protected set; }
+  public int X { get; protected set; }
+  public int Y { get; protected set; }
 
-	public float movementCost {
-		get {
+  public float movementCost {
+    get {
 
-			if(Type == TileType.Empty)
-				return 0;	// 0 is unwalkable
+      if (Type == TileType.Empty)
+        return 0; // 0 is unwalkable
 
-			if(furniture == null)
-				return 1;
+      if (furniture == null)
+        return 1;
 
-			return 1 * furniture.movementCost;
-		}
-	}
+      return 1 * furniture.movementCost;
+    }
+  }
 
-	// The function we callback any time our tile's data changes
-	Action<Tile> cbTileChanged;
+  // The function we callback any time our tile's data changes
+  Action<Tile> cbTileChanged;
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="Tile"/> class.
-	/// </summary>
-	/// <param name="world">A World instance.</param>
-	/// <param name="x">The x coordinate.</param>
-	/// <param name="y">The y coordinate.</param>
-	public Tile( World world, int x, int y ) {
-		this.world = world;
-		this.X = x;
-		this.Y = y;
-	}
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Tile"/> class.
+  /// </summary>
+  /// <param name="world">A World instance.</param>
+  /// <param name="x">The x coordinate.</param>
+  /// <param name="y">The y coordinate.</param>
+  public Tile(World world, int x, int y) {
+    this.world = world;
+    this.X = x;
+    this.Y = y;
+  }
 
-	/// <summary>
-	/// Register a function to be called back when our tile type changes.
-	/// </summary>
-	public void RegisterTileTypeChangedCallback(Action<Tile> callback) {
-		cbTileChanged += callback;
-	}
-	
-	/// <summary>
-	/// Unregister a callback.
-	/// </summary>
-	public void UnregisterTileTypeChangedCallback(Action<Tile> callback) {
-		cbTileChanged -= callback;
-	}
+  /// <summary>
+  /// Register a function to be called back when our tile type changes.
+  /// </summary>
+  public void RegisterTileTypeChangedCallback(Action<Tile> callback) {
+    cbTileChanged += callback;
+  }
 
-	public bool PlaceFurniture(Furniture objInstance) {
-		if(objInstance == null) {
-			// We are uninstalling whatever was here before.
-			furniture = null;
-			return true;
-		}
+  /// <summary>
+  /// Unregister a callback.
+  /// </summary>
+  public void UnregisterTileTypeChangedCallback(Action<Tile> callback) {
+    cbTileChanged -= callback;
+  }
 
-		// objInstance isn't null
+  public bool PlaceFurniture(Furniture objInstance) {
+    if (objInstance == null) {
+      // We are uninstalling whatever was here before.
+      furniture = null;
+      return true;
+    }
 
-		if(furniture != null) {
-			Debug.LogError("Trying to assign a furniture to a tile that already has one!");
-			return false;
-		}
+    // objInstance isn't null
 
-		// At this point, everything's fine!
+    if (furniture != null) {
+      Debug.LogError("Trying to assign a furniture to a tile that already has one!");
+      return false;
+    }
 
-		furniture = objInstance;
-		return true;
-	}
+    // At this point, everything's fine!
 
-	// Tells us if two tiles are adjacent.
-	public bool IsNeighbour(Tile tile, bool diagOkay = false) {
-		// Check to see if we have a difference of exactly ONE between the two
-		// tile coordinates.  Is so, then we are vertical or horizontal neighbours.
-		return 
-			Mathf.Abs( this.X - tile.X ) + Mathf.Abs( this.Y - tile.Y ) == 1 ||  // Check hori/vert adjacency
-			( diagOkay && ( Mathf.Abs( this.X - tile.X ) == 1 && Mathf.Abs( this.Y - tile.Y ) == 1 ) ) // Check diag adjacency
-			;
-	}
+    furniture = objInstance;
+    return true;
+  }
 
-	/// <summary>
-	/// Gets the neighbours.
-	/// </summary>
-	/// <returns>The neighbours.</returns>
-	/// <param name="diagOkay">Is diagonal movement okay?.</param>
-	public Tile[] GetNeighbours(bool diagOkay = false) {
-		Tile[] ns;
+  // Tells us if two tiles are adjacent.
+  public bool IsNeighbour(Tile tile, bool diagOkay = false) {
+    // Check to see if we have a difference of exactly ONE between the two
+    // tile coordinates.  Is so, then we are vertical or horizontal neighbours.
+    return
+      Mathf.Abs(this.X - tile.X) + Mathf.Abs(this.Y - tile.Y) == 1 ||  // Check hori/vert adjacency
+      (diagOkay && (Mathf.Abs(this.X - tile.X) == 1 && Mathf.Abs(this.Y - tile.Y) == 1)) // Check diag adjacency
+      ;
+  }
 
-		if(diagOkay == false) {
-			ns = new Tile[4];	// Tile order: N E S W
-		}
-		else {
-			ns = new Tile[8];	// Tile order : N E S W NE SE SW NW
-		}
+  /// <summary>
+  /// Gets the neighbours.
+  /// </summary>
+  /// <returns>The neighbours.</returns>
+  /// <param name="diagOkay">Is diagonal movement okay?.</param>
+  public Tile[] GetNeighbours(bool diagOkay = false) {
+    Tile[] ns;
 
-		Tile n;
+    if (diagOkay == false) {
+      ns = new Tile[4]; // Tile order: N E S W
+    } else {
+      ns = new Tile[8]; // Tile order : N E S W NE SE SW NW
+    }
 
-		n = world.GetTileAt(X, Y+1);
-		ns[0] = n;	// Could be null, but that's okay.
-		n = world.GetTileAt(X+1, Y);
-		ns[1] = n;	// Could be null, but that's okay.
-		n = world.GetTileAt(X, Y-1);
-		ns[2] = n;	// Could be null, but that's okay.
-		n = world.GetTileAt(X-1, Y);
-		ns[3] = n;	// Could be null, but that's okay.
+    Tile n;
 
-		if(diagOkay == true) {
-			n = world.GetTileAt(X+1, Y+1);
-			ns[4] = n;	// Could be null, but that's okay.
-			n = world.GetTileAt(X+1, Y-1);
-			ns[5] = n;	// Could be null, but that's okay.
-			n = world.GetTileAt(X-1, Y-1);
-			ns[6] = n;	// Could be null, but that's okay.
-			n = world.GetTileAt(X-1, Y+1);
-			ns[7] = n;	// Could be null, but that's okay.
-		}
+    n = world.GetTileAt(X, Y + 1);
+    ns[0] = n;  // Could be null, but that's okay.
+    n = world.GetTileAt(X + 1, Y);
+    ns[1] = n;  // Could be null, but that's okay.
+    n = world.GetTileAt(X, Y - 1);
+    ns[2] = n;  // Could be null, but that's okay.
+    n = world.GetTileAt(X - 1, Y);
+    ns[3] = n;  // Could be null, but that's okay.
 
-		return ns;
-	}
+    if (diagOkay == true) {
+      n = world.GetTileAt(X + 1, Y + 1);
+      ns[4] = n;  // Could be null, but that's okay.
+      n = world.GetTileAt(X + 1, Y - 1);
+      ns[5] = n;  // Could be null, but that's okay.
+      n = world.GetTileAt(X - 1, Y - 1);
+      ns[6] = n;  // Could be null, but that's okay.
+      n = world.GetTileAt(X - 1, Y + 1);
+      ns[7] = n;  // Could be null, but that's okay.
+    }
+
+    return ns;
+  }
 
 }
